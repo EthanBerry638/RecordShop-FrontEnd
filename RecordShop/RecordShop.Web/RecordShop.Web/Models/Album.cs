29d6789c4c85d406b8aa0c9f84490d66cb8bfe73
Ethan Berry <ethanberry670@gmail.com>
2026-05-18 15:37:29 +0100

    Feat: Add data annotations for album

--- RecordShop/RecordShop.Web/RecordShop.Web/Models/Album.cs
+++ RecordShop/RecordShop.Web/RecordShop.Web/Models/Album.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace RecordShop.Web.Models
 {
@@ -8,15 +9,18 @@ namespace RecordShop.Web.Models
         public int Id { get; set; }
 
         [JsonPropertyName("title")]
+        [Required][MaxLength(150, ErrorMessage = "Title cannot exceed 150 characters")]
         public string Title { get; set; } = string.Empty;
 
         [JsonPropertyName("description")]
+        [Required][MaxLength(250, ErrorMessage = "Description cannot exceed 250 characters")]
         public string Description { get; set; } = string.Empty;
 
         [JsonPropertyName("releasedate")]
         public DateOnly? ReleaseDate { get; set; }
 
         [JsonPropertyName("price")]
+        [Required][Range(0.01, 2000000.00, ErrorMessage = "Price must be between 0.01 and 2,000,000")]
         public decimal Price { get; set; }
     }
 }
