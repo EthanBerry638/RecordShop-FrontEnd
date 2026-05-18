220c7c4b5f293658d1ccfe2efc29c3cb9b2de4b1
Ethan Berry <ethanberry670@gmail.com>
2026-05-18 14:00:34 +0100

    Fix: Now fetching data correctly from API using annotations

--- RecordShop/RecordShop.Web/RecordShop.Web/Models/Album.cs
+++ RecordShop/RecordShop.Web/RecordShop.Web/Models/Album.cs
@@ -1,11 +1,22 @@
-﻿namespace RecordShop.Web.Models
+﻿using System.Text.Json.Serialization;
+
+namespace RecordShop.Web.Models
 {
     public class Album
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("title")]
         public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("description")]
         public string Description { get; set; } = string.Empty;
+
+        [JsonPropertyName("releasedate")]
         public DateOnly? ReleaseDate { get; set; }
+
+        [JsonPropertyName("price")]
         public decimal Price { get; set; }
     }
 }
