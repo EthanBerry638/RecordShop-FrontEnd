864d145332acfd674ad5d6b758275e5977426407
Ethan Berry <ethanberry670@gmail.com>
2026-05-20 14:26:23 +0100

    Feat: Add base url string

--- RecordShop/RecordShop.Web/RecordShop.Web/Services/AlbumService.cs
+++ RecordShop/RecordShop.Web/RecordShop.Web/Services/AlbumService.cs
@@ -3,5 +3,7 @@
     public class AlbumService (HttpClient httpClient)
     {
         private readonly HttpClient _httpClient = httpClient;
+
+        private const string _baseApiUrl = "https://localhost:7091/api/Album";
     }
 }
