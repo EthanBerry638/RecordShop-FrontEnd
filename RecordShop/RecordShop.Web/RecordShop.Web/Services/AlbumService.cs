589e550289339633aea478190d00ddb4e60fb186
Ethan Berry <ethanberry670@gmail.com>
2026-05-20 14:32:05 +0100

    Feat: Add get all albums async

--- RecordShop/RecordShop.Web/RecordShop.Web/Services/AlbumService.cs
+++ RecordShop/RecordShop.Web/RecordShop.Web/Services/AlbumService.cs
@@ -1,9 +1,16 @@
-﻿namespace RecordShop.Web.Services
+﻿using RecordShop.Web.Models;
+
+namespace RecordShop.Web.Services
 {
     public class AlbumService (HttpClient httpClient)
     {
         private readonly HttpClient _httpClient = httpClient;
 
         private const string _baseApiUrl = "https://localhost:7091/api/Album";
+
+        public async Task<List<Album>?> GetAllAlbumsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Album>>(_baseApiUrl);
+        }
     }
 }
